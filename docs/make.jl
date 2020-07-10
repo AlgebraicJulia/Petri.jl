013075e6b96e7a879a58198d8ee020898408da4b
Micah Halter <micah.halter@gtri.gatech.edu>
2020-07-10 08:45:18 -0400

    Documentation Update (#28)
    * Updated docs to use Literate.jl to generate example notebooks
    
    * Update README and docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,46 @@
 using Documenter
+using Literate
 
 @info "Loading Petri"
 using Petri
 
+# Set Literate.jl config if not being compiled on recognized service.
+config = Dict{String,String}()
+if !(haskey(ENV, "GITHUB_ACTIONS") || haskey(ENV, "GITLAB_CI"))
+  config["nbviewer_root_url"] = "https://nbviewer.jupyter.org/github/mehalter/Petri.jl/blob/gh-pages/dev"
+  config["repo_root_url"] = "https://github.com/mehalter/Petri.jl/blob/master/docs"
+end
+
+const literate_dir = joinpath(@__DIR__, "..", "examples")
+const generated_dir = joinpath(@__DIR__, "src", "examples")
+
+for (root, dirs, files) in walkdir(literate_dir)
+  out_dir = joinpath(generated_dir, relpath(root, literate_dir))
+  for file in files
+    f,l = splitext(file)
+    if l == ".jl" && !startswith(f, "_")
+      Literate.markdown(joinpath(root, file), out_dir;
+        config=config, documenter=true, credit=false)
+      Literate.notebook(joinpath(root, file), out_dir;
+        execute=true, documenter=true, credit=false)
+    end
+  end
+end
+
 @info "Building Documenter.jl docs"
 makedocs(
   modules   = [Petri],
-  format    = Documenter.HTML(),
+  format    = Documenter.HTML(
+    assets = ["assets/analytics.js"],
+  ),
   sitename  = "Petri.jl",
   doctest   = false,
   checkdocs = :none,
   pages     = Any[
     "Petri.jl" => "index.md",
-    "Basic Usage" => "usage.md",
+    "Examples" => Any[
+      "examples/epidemiology.md",
+    ],
     "Library Reference" => "api.md",
   ]
 )
