6feac0e654b4368615232c51a5943bb86cef2d24
Micah Halter <micah.halter@gtri.gatech.edu>
2020-07-13 13:55:10 -0400

    Added support for SteadyStateProblems and JumpProblems (#29)
    * Added support for SteadStateProblems and JumpProblems
    
    * Fixed compat entries so packages are upgradable

--- test/solvers.jl
+++ test/solvers.jl
@@ -3,55 +3,34 @@ using Petri
 using LabelledArrays
 using OrdinaryDiffEq
 using StochasticDiffEq
+using DiffEqJump
+using Random
+
+Random.seed!(1234);
 
 @testset "Generation of ODE formulas" begin
     sir = Petri.Model([:S,:I,:R],[(LVector(S=1,I=1), LVector(I=2)),
                                   (LVector(I=1),     LVector(R=1))])
 
     sirf = vectorfield(sir)
-    @test sirf(LVector(S=0.0, I=0.0, R=0.0), LVector(S=100.0,I=1.0,R=0.0), [0.35,0.05],0.0) == LVector(S=-35.0,I=34.95,R=0.05)
+    rates = sirf(LVector(S=0.0, I=0.0, R=0.0), LVector(S=100.0,I=1.0,R=0.0), [0.5/101,0.25],0.0)
+    @test rates.S ≈ -.495 atol=1e-3
+    @test rates.I ≈ .24505 atol=1e-5
+    @test rates.R ≈ .25 atol=1e-2
 end
 
 @testset "Generation of ODE solutions" begin
     @testset "SIR Solving" begin
         sir = Petri.Model([:S,:I,:R],[(LVector(S=1,I=1), LVector(I=2)),
                                       (LVector(I=1),     LVector(R=1))])
-        u0 = LVector(S=100.0,I=1.0,R=0.0)
-        p = [0.35, 0.05]
-        prob = ODEProblem(sir,u0,(0.0,365.0),p)
+        u0 = LVector(S=990.0,I=10.0,R=0.0)
+        p = [0.5/sum(u0), 0.25]
+        prob = ODEProblem(sir,u0,(0.0,40.0),p)
         sol = OrdinaryDiffEq.solve(prob,Tsit5())
-        @test sol[end].S < 1
-        @test sol[end].I < 1
-        @test sol[end].R > 99
-    end
-
-    @testset "SEIR Solving" begin
-        seir = Petri.Model([:S,:E,:I,:R],LVector(exp=(LVector(S=1,I=1), LVector(I=1,E=1)),
-                                                 inf=(LVector(E=1),       LVector(I=1)),
-                                                 rec=(LVector(I=1),       LVector(R=1))))
-        u0 = LVector(S=100.0, E=1.0, I=0.0, R=0.0)
-        p = LVector(exp=0.35, inf=0.05, rec=0.05)
-        prob = ODEProblem(seir,u0,(0.0,365.0),p)
-        sol = OrdinaryDiffEq.solve(prob,Tsit5())
-        @test sol[end].S < 1
-        @test sol[end].E < 1
-        @test sol[end].I < 1
-        @test sol[end].R > 99
-    end
-
-    @testset "SEIRS Solving" begin
-        seirs = Petri.Model([:S,:E,:I,:R],LVector(exp=(LVector(S=1,I=1), LVector(I=1,E=1)),
-                                                  inf=(LVector(E=1),     LVector(I=1)),
-                                                  rec=(LVector(I=1),     LVector(R=1)),
-                                                  deg=(LVector(R=1),     LVector(S=1))))
-        u0 = LVector(S=100.0, E=1.0, I=0.0, R=0.0)
-        p = LVector(exp=0.3, inf=0.4, rec=0.01, deg=0.01)
-        prob = ODEProblem(seirs,u0,(0.0,60.0),p)
-        sol = OrdinaryDiffEq.solve(prob,Tsit5())
-        @test sol[end].S < 1
-        @test sol[end].E < 1
-        @test sol[end].I > 60
-        @test sol[end].R > 30
+        @test sum(sol[end]) ≈ 1000 atol=1e-3
+        @test sol[end].S ≈ 209.843 atol=1e-3
+        @test sol[end].I ≈ 14.474 atol=1e-3
+        @test sol[end].R ≈ 775.684 atol=1e-3
     end
 end
 
@@ -59,41 +38,28 @@ end
     @testset "SIR Solving" begin
         sir = Petri.Model([:S,:I,:R],[(LVector(S=1,I=1), LVector(I=2)),
                                       (LVector(I=1),     LVector(R=1))])
-        u0 = LVector(S=100.0,I=1.0,R=0.0)
-        p = [0.35, 0.05]
-        prob,cb = SDEProblem(sir,u0,(0.0,365.0),p)
+        u0 = LVector(S=990.0,I=10.0,R=0.0)
+        p = [0.5/sum(u0), 0.25]
+        prob,cb = SDEProblem(sir,u0,(0.0,40.0),p)
         sol = StochasticDiffEq.solve(prob,SRA1(),callback=cb)
-        @test sol[end].S < 1.1
-        @test sol[end].I < 1.1
-        @test sol[end].R > 98.9
+        @test sum(sol[end]) ≈ 1000 atol=1e-3
+        @test sol[end].S ≈ 186.054 atol=1e-3
+        @test sol[end].I ≈ 13.447 atol=1e-3
+        @test sol[end].R ≈ 800.499 atol=1e-3
     end
+end
 
-    @testset "SEIR Solving" begin
-        seir = Petri.Model([:S,:E,:I,:R],LVector(exp=(LVector(S=1,I=1), LVector(I=1,E=1)),
-                                                 inf=(LVector(E=1),       LVector(I=1)),
-                                                 rec=(LVector(I=1),       LVector(R=1))))
-        u0 = LVector(S=100.0, E=1.0, I=0.0, R=0.0)
-        p = LVector(exp=0.35, inf=0.05, rec=0.05)
-        prob,cb = SDEProblem(seir,u0,(0.0,365.0),p)
-        sol = StochasticDiffEq.solve(prob,SRA1(),callback=cb)
-        @test sol[end].S < 1.1
-        @test sol[end].E < 1.1
-        @test sol[end].I < 1.1
-        @test sol[end].R > 98.9
-    end
-
-    @testset "SEIRS Solving" begin
-        seirs = Petri.Model([:S,:E,:I,:R],LVector(exp=(LVector(S=1,I=1), LVector(I=1,E=1)),
-                                                  inf=(LVector(E=1),     LVector(I=1)),
-                                                  rec=(LVector(I=1),     LVector(R=1)),
-                                                  deg=(LVector(R=1),     LVector(S=1))))
-        u0 = LVector(S=100.0, E=1.0, I=0.0, R=0.0)
-        p = LVector(exp=0.3, inf=0.4, rec=0.01, deg=0.01)
-        prob,cb = SDEProblem(seirs,u0,(0.0,60.0),p)
-        sol = StochasticDiffEq.solve(prob,SRA1(),callback=cb)
-        @test sol[end].S < 1.1
-        @test sol[end].E < 1.1
-        @test sol[end].I > 59.9
-        @test sol[end].R > 29.9
+@testset "Generation of Jump solutions" begin
+    @testset "SIR Solving" begin
+        sir = Petri.Model([:S,:I,:R],[(LVector(S=1,I=1), LVector(I=2)),
+                                      (LVector(I=1),     LVector(R=1))])
+        u0 = LVector(S=990.0,I=10.0,R=0.0)
+        p = [0.5/sum(u0), 0.25]
+        prob = JumpProblem(sir,u0,(0.0,40.0),p)
+        sol = DiffEqJump.solve(prob,SSAStepper())
+        @test sum(sol[end]) == 1000
+        @test sol[end].S == 263
+        @test sol[end].I == 10
+        @test sol[end].R == 727
     end
 end
